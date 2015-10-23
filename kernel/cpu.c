45507c5a00346f2c723e035abac6f033085200af
Alok Nikhil <nikhil.jan93@ufl.edu>
2015-10-23 00:21:05 -0400

    X-Plug: Port X-Plug from Honami
    Change-Id: Ib9f31c71a3cd44578049263c2c0f0535036ac8d9

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -371,8 +371,8 @@ int __ref cpu_down(unsigned int cpu)
 
 	cpu_maps_update_begin();
 
-	// AP: Keep CPU cores 0 and 4 always on
-	if ((cpu == 0) || (cpu == 4))
+	// AP: Keep CPU core 0 on
+	if (cpu == 0)
 	{
 		err = -EBUSY;
 		goto out;
